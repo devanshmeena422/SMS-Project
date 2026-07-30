cb7dacf5fb8d4fa57ef32ee5954acb3feef1b56b
Devansh Meena <devanshmeena422@gmail.com>
2026-07-30 22:33:18 +0530

    Fix case sensitivity in controller imports for payment and subscription routes

--- Subscription Management System/BACKEND/routes/paymentroutes.js
+++ Subscription Management System/BACKEND/routes/paymentroutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const router=express.Router();
 
-const paymentController=require("../controllers/paymentController");
+const paymentController=require("../controllers/paymentcontroller");
 
 router.get("/",paymentController.getPayments);
 
--- Subscription Management System/BACKEND/routes/subscriptionRoutes.js
+++ Subscription Management System/BACKEND/routes/subscriptionRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const router=express.Router();
 
-const subscriptionController=require("../controllers/subscriptionController");
+const subscriptionController=require("../controllers/subscriptioncontroller");
 
 router.get("/",subscriptionController.getSubscriptions);
 
