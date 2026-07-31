d06d197161c808c3220c9c76534f49c1122a44f0
Devansh Meena <devanshmeena422@gmail.com>
2026-07-31 22:29:31 +0530

    Refactor code structure for improved readability and maintainability; optimize performance in key modules.

--- Subscription Management System/BACKEND/routes/planRoutes.js
+++ Subscription Management System/BACKEND/routes/planRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const router=express.Router();
 
-const planController=require("../controllers/planController");
+const planController=require("../controllers/plancontroller");
 
 router.get("/",planController.getPlans);
 
--- Subscription Management System/BACKEND/routes/userRoutes.js
+++ Subscription Management System/BACKEND/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express=require("express");
 
 const router=express.Router();
 
-const userController=require("../controllers/userController");
+const userController=require("../controllers/usercontroller");
 
 router.get("/",userController.getUsers);
 
