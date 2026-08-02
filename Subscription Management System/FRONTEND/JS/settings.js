a1559145238280152eb4cd152056e627f2bfb9b6
Devansh Meena <devanshmeena422@gmail.com>
2026-08-02 12:22:29 +0530

    Add JavaScript functionality to handle settings form submission and display success alert

---
+++ Subscription Management System/FRONTEND/JS/settings.js
@@ -0,0 +1,9 @@
+const settingsForm=document.getElementById("settingsForm");
+
+settingsForm.addEventListener("submit",function(event){
+
+event.preventDefault();
+
+alert("Settings Saved Successfully");
+
+});
