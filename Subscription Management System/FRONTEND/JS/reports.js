efa7c47176648809328d3c61c369c047a715754d
Devansh Meena <devanshmeena422@gmail.com>
2026-08-02 12:20:45 +0530

    Add JavaScript functionality for report download and admin page navigation

---
+++ Subscription Management System/FRONTEND/JS/reports.js
@@ -0,0 +1,14 @@
+const downloadReport=document.getElementById("downloadReport");
+const adminPage=document.getElementById("adminPage");
+
+downloadReport.addEventListener("click",function(){
+
+alert("Report Download Started");
+
+});
+
+adminPage.addEventListener("click",function(){
+
+window.location.href="admin.html";
+
+});
